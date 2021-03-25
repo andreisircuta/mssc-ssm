1e92e08e3b925d9572b219da569166cf75e22c04
andrei.sircuta <andrei.sircuta@gmail.com>
2021-03-25 14:51:43 +0200

    Init the state machine from the database

--- src/main/java/guru/springframework/services/PaymentServiceImpl.java
+++ src/main/java/guru/springframework/services/PaymentServiceImpl.java
@@ -7,6 +7,7 @@ import guru.springframework.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,16 +24,37 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuthorize(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> stateMachine = build(paymentId);
+
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> stateMachine = build(paymentId);
+
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuthorization(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> stateMachine = build(paymentId);
+
         return null;
+    }
+
+    private StateMachine<PaymentState, PaymentEvent> build(Long paymentId){
+        Payment payment = paymentRepository.getOne(paymentId);
+        StateMachine<PaymentState, PaymentEvent> stateMachine = stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+
+        stateMachine.stop();
+        //set the state machine state to be the state from the databese
+        stateMachine.getStateMachineAccessor()
+                .doWithAllRegions(stateMachineAccessor ->{
+                    stateMachineAccessor.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null));
+                });
+        stateMachine.start();
+
+        return stateMachine;
     }
 }
